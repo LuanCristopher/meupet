84cca9a630ca629f4401d11255e66496c842ad97
luanOliveiraSauter <lcoliveira@sauter.digital>
2024-08-05 15:26:31 -0300

    Segundo Commit -resta apenas consult

---
+++ script/script.js
@@ -0,0 +1,3 @@
+document.getElementById('btn').addEventListener('click', () => {
+    alert('A DEPA permite o registro de Denúncias de crimes contra animais O registro de um boletim de ocorrência deve ser feito na unidade policial mais próxima.');
+});
