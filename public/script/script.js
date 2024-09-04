f381c87a53fb0dce7b2f29da82f1092f89ddee8f
LuanCristopher <125354837+LuanCristopher@users.noreply.github.com>
2024-09-04 18:15:01 -0300

    Update script.js

--- public/script/script.js
+++ public/script/script.js
@@ -1,11 +1,13 @@
 document.getElementById('denunciaForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Impede o envio do formulário
-
+    
+    /*
     // Pega o texto da denúncia
     const denunciaText = document.getElementById('denunciaText').value;
 
     // Salva no localStorage
     localStorage.setItem('denuncia', denunciaText);
+    */
 
     // Redireciona para a página de consulta
     window.location.href = './consut.html';
